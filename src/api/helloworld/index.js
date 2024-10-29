f7746073573b58917d94d52c1ad08875a74b69a5
Khriztian Moreno <khriztianmoreno@gmail.com>
2024-10-29 18:50:19 -0500

    release 1 (#3)
    * fix: :bug: update code style (#1)
    
    * feat: :sparkles: create task endpoint (#2)

--- src/api/helloworld/index.js
+++ src/api/helloworld/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (_, res) => {
-  res.json({ message: 'hello world'})
+  res.json({ message: 'hello world' });
 });
 
 module.exports = router;
