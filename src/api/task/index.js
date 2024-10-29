e604bb7def2cf24b56b38e4ac88856bc01f03875
Khriztian Moreno <khriztianmoreno@gmail.com>
2024-10-29 18:44:03 -0500

    fix: :bug: update code style (#1)

--- src/api/task/index.js
+++ src/api/task/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (_, res) => {
-  res.json({ message: 'task'})
+  res.json({ message: 'task' });
 });
 
 module.exports = router;
